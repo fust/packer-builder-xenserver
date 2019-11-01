0a5d5141f2ab8d73f1334cf710b88f100663c925
Thom Wijtenburg <thom.wijtenburg@kvk.nl>
2019-11-01 14:43:58 +0100

    [WIP] add convert to template step

---
+++ builder/xenserver/common/step_convert_to_template.go
@@ -0,0 +1,46 @@
+package common
+
+import (
+	"fmt"
+
+	"github.com/mitchellh/multistep"
+	"github.com/mitchellh/packer/packer"
+	xsclient "github.com/xenserver/go-xenserver-client"
+)
+
+type StepConvertToTemplate struct{}
+
+func (self *StepConvertToTemplate) Run(state multistep.StateBag) multistep.StepAction {
+	config := state.Get("commonconfig").(CommonConfig)
+	ui := state.Get("ui").(packer.Ui)
+	client := state.Get("client").(xsclient.XenAPIClient)
+	instance_uuid := state.Get("instance_uuid").(string)
+
+	instance, err := client.GetVMByUuid(instance_uuid)
+	if err != nil {
+		ui.Error(fmt.Sprintf("Could not get VM with UUID '%s': %s", instance_uuid, err.Error()))
+		return multistep.ActionHalt
+	}
+
+	ui.Say("Step: convert VM to template")
+
+	if config.Convert == false || config.Convert == "" {
+		ui.Say("Skipping conversion")
+		return multistep.ActionContinue
+	}
+
+	success := func() bool {
+		ui.Message("Converting VM to template")
+		instance.SetIsATemplate(true)
+	}
+
+	if !success {
+		ui.Error(fmt.Sprintf("Could convert VM to template"))
+		return multistep.ActionHalt
+	}
+
+	ui.Message("Successfully converted VM to template")
+	return multistep.ActionContinue
+}
+
+func (StepConvertToTemplate) Cleanup(state multistep.StateBag) {}
